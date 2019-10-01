d159a5023c3b42a9576c9882c37a23d6b316f513
GianBarGian <giacomobenati@hotmail.it>
2019-10-01 15:55:01 +0200

    fisrt implementation of is_prim func

--- src/16_prime.py
+++ src/16_prime.py
@@ -1,4 +1,17 @@
 import sys
-
+import math
 num = input("Enter a number: ")
 num = int(num)
+
+start = 2
+
+def is_prime(num):
+    m = 2
+    while m < math.sqrt(num):
+        print(m)
+        if num % m == 0:
+            return False
+        m += 1
+    return True
+
+print(is_prime(num))
