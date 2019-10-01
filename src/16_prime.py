aa492558e4758177659096e36d7d1b21df499b0c
GianBarGian <giacomobenati@hotmail.it>
2019-10-01 16:51:18 +0200

    improved is_prime func

--- src/16_prime.py
+++ src/16_prime.py
@@ -3,15 +3,19 @@ import math
 num = input("Enter a number: ")
 num = int(num)
 
-start = 2
 
 def is_prime(num):
     m = 2
-    while m < math.sqrt(num):
-        print(m)
+    k = 1
+    while m < 4:
         if num % m == 0:
             return False
         m += 1
+    while 6 * k - 1 < math.sqrt(num):
+        if num % (6 * k + 1) == 0 or num % (6 * k - 1) == 0:
+            return False
+        k += 1
     return True
+
 
 print(is_prime(num))
