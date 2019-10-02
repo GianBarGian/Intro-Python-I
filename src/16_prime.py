7945605843e92758df061e4a4e614ce26ec7d922
GianBarGian <giacomobenati@hotmail.it>
2019-10-02 09:21:44 +0200

    creating the new version of is_prime (wip)

--- src/16_prime.py
+++ src/16_prime.py
@@ -19,3 +19,13 @@ def is_prime(num):
 
 
 print(is_prime(num))
+
+def is_prime_improve(num):
+    primes =  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 200]
+    for prime in primes:
+        print(prime)
+        if prime < num and num % prime == 0:
+            return False
+    return True
+
+print(is_prime_improve(num))
